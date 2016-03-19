da57599310a5193646455fe267f6370b8a7128b6
Tonino Catapano <t.catapano@tmg.nl>
2016-03-19 09:37:45 +0100

    initial commit (video 9)

---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,54 @@
+var React = require('react');
+
+var Prompt = require('../components/Prompt');
+
+
+var PromptContainer = React.createClass({
+  contextTypes:{
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState() {
+    return {
+      username: ''
+    };
+  },
+  handleUpdateUser : function(e){
+    this.setState({
+      username: e.target.value
+    })
+  },
+  handleSubmitUser : function(e){
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    })
+    if(this.props.routeParams.playerOne){
+      // we are on the player two page, goto Battle!
+      this.context.router.push({
+        pathname : '/battle',
+        query :{
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        },
+      });
+
+    }else{
+      // we are on the player two page, goto player 2
+      this.context.router.push('/playerTwo/' + this.state.username);
+    }
+  },
+  render: function(){
+    return(
+      <Prompt onSubmitUser={this.handleSubmitUser}
+              onUpdateUser={this.handleUpdateUser}
+              header={this.props.route.header}
+              username={this.state.username}/>
+    )
+  }
+})
+
+
+
+
+module.exports = PromptContainer;
