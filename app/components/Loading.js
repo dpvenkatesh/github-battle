d0b4dab2c999742c8212498d9c9245a383ccdc4b
Tonino Catapano <t.catapano@tmg.nl>
2016-03-20 11:58:08 +0100

    started video 11

---
+++ app/components/Loading.js
@@ -0,0 +1,55 @@
+var React = require('react');
+
+var styles = {
+  container:{
+    position: 'fixed',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    fontSize: '55px'
+  },
+  content: {
+    textAlign: 'center',
+    position: 'absolute',
+    width: '100%',
+    marginTop: '30px'
+  }
+};
+
+var Loading = React.createClass({
+  getInitialState() {
+    this.originalText = 'Loading';
+    return {
+      text: 'Loading'
+    };
+  },
+  componentDidMount() {
+    var stopper = 'this.originalText + '...';
+    this.interval = setInterval(function(){
+      if(this.state.text === stopper){
+        this.setState({
+          text: this.originalText
+        })
+      }else{
+        this.setState({
+          text: this.state.text + '.';
+        })
+      }
+    }.bind(this),300)
+  },
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  },
+
+  render : function(){
+    return (
+      <div style={styles.container}>
+        <p style={styles.content}>{this.state.text}</p>
+      </div>
+    )
+  }
+
+});
+
+module.exports = loading;
